7f212ebb080af2887001a0e47fce7129cb21c6fb
Brandon Cronin <brandon.cronin@gmail.com>
2018-05-02 15:09:34 -0700

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashbaordComponent } from './dashbaord/dashbaord.component';
+import { CustomerComponent } from './customer/customer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', component: DashbaordComponent },
+  { path: 'customer', component: CustomerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { DashbaordComponent } from './dashbaord/dashbaord.component';
+import { CustomerComponent } from './customer/customer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashbaordComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
